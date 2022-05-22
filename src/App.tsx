6ba0293bcb9f5580773e79d2e7abd742c2779b56
Nic <42087659+nicm42@users.noreply.github.com>
2022-05-22 14:30:15 +0100

    Deleted commented out bits

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-// import { useState } from 'react';
 import Card from './components/Card';
 import cats from './cats';
-// import logo from './logo.svg'
-// import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0);
-  // console.log(cats);
-
   return (
     <div className="App">
       {cats.map((cat) => (
