d48333120140c3e160f56e2da14dad377a6290e9
Nic <42087659+nicm42@users.noreply.github.com>
2022-05-29 17:58:42 +0100

    Changed the font import

--- src/theme.css.ts
+++ src/theme.css.ts
@@ -4,12 +4,23 @@ import {
   globalStyle,
 } from '@vanilla-extract/css';
 
+// TODO update fonts only work on build, not on dev
+// Might help: https://stackoverflow.com/questions/70086712/load-local-fonts-in-vite-vue3-project
+
+// TODO once fonts are sorted move variables into a vars.css file
+
 globalFontFace('Ubuntu', {
-  src: 'url(./fonts/Ubuntu-Regular.ttf)',
+  // src: 'local(./fonts/Ubuntu-Regular.ttf)',
+  // src: 'url(./fonts/Ubuntu-Regular.ttf)',
+  // src: "local('Ubuntu-Regular'), url(./fonts/Ubuntu-Regular.ttf)",
+  src: 'local("Ubuntu-Regular"), url(@/fonts/Ubuntu-Regular.ttf) format("truetype")',
 });
 
 globalFontFace('ShadowsIntoLight', {
-  src: 'url(./fonts/ShadowsIntoLightTwo-Regular.ttf)',
+  // src: 'local(./fonts/ShadowsIntoLightTwo-Regular.ttf)',
+  // src: 'url(./fonts/ShadowsIntoLightTwo-Regular.ttf)',
+  // src: "local('ShadowsIntoLightTwo-Regular'), url(./fonts/ShadowsIntoLightTwo-Regular.ttf)",
+  src: 'local("ShadowsIntoLightTwo-Regular"), url(@/fonts/ShadowsIntoLightTwo-Regular.ttf) format("truetype")',
 });
 
 export const vars = createGlobalTheme(':root', {
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
