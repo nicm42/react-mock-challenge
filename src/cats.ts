b725be90712af43df14af212364253701a8b7287
Nic <42087659+nicm42@users.noreply.github.com>
2022-06-04 20:25:57 +0100

    Made all the texts different lengths

--- src/cats.ts
+++ src/cats.ts
@@ -9,64 +9,73 @@ const CATS = [
   {
     name: 'Blizzard',
     breed: 'Calico',
-    url: 'https://placekitten.com/801/601',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    url: 'https://placekitten.com/810/610',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit inventore voluptatibus',
     age: 7,
   },
   {
     name: 'Garfield',
     breed: 'Main coon',
-    url: 'https://placekitten.com/802/602',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    url: 'https://placekitten.com/820/620',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit inventore voluptatibus, at corrupti eligendi cum enim',
     age: 4,
   },
   {
     name: 'Vanilla',
     breed: 'Siberian',
-    url: 'https://placekitten.com/803/603',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    url: 'https://placekitten.com/830/630',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit inventore voluptatibus, at corrupti eligendi cum enim ad saepe assumenda incidunt architecto quae deleniti',
     age: 1,
   },
   {
     name: 'Savannah',
     breed: 'Scottish fold',
-    url: 'https://placekitten.com/804/604',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    url: 'https://placekitten.com/840/640',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit inventore voluptatibus, at corrupti eligendi cum enim ad saepe assumenda incidunt architecto quae deleniti ab voluptate in suscipit aliquam!',
     age: 10,
   },
   {
     name: 'Sudoku',
     breed: 'Munchkin',
-    url: 'https://placekitten.com/805/605',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    url: 'https://placekitten.com/850/650',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit inventore voluptatibus, at corrupti eligendi cum enim ad saepe assumenda incidunt architecto quae deleniti ab voluptate in suscipit aliquam! Laboriosam, nam!',
     age: 2,
   },
   {
     name: 'Brownie',
     breed: 'Ragdoll',
-    url: 'https://placekitten.com/806/606',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    url: 'https://placekitten.com/860/660',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit inventore voluptatibus, at corrupti eligendi cum enim ad saepe assumenda incidunt architecto quae deleniti ab voluptate in suscipit aliquam!',
     age: 9,
   },
   {
     name: 'Ms Aloof',
     breed: 'British Shorthair',
-    url: 'https://placekitten.com/807/607',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    url: 'https://placekitten.com/870/670',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit inventore voluptatibus, at corrupti eligendi cum enim ad saepe assumenda incidunt architecto quae deleniti',
     age: 4,
   },
   {
     name: 'Winnie',
     breed: 'Sphynx',
-    url: 'https://placekitten.com/808/608',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    url: 'https://placekitten.com/880/680',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit inventore voluptatibus, at corrupti eligendi cum enim',
     age: 2,
   },
   {
     name: 'Puffins',
     breed: 'Bengal',
-    url: 'https://placekitten.com/809/609',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    url: 'https://placekitten.com/890/690',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit inventore voluptatibus',
     age: 6,
   },
 ];
