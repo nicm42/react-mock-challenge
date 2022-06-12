4a342e48de2fcc964496bb57727668d845ff8eb7
Nic <42087659+nicm42@users.noreply.github.com>
2022-06-12 17:43:56 +0100

    Tested cards are on page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,25 @@
-import { render } from '@testing-library/react';
-import { JSDOM } from 'jsdom';
+/**
+ *  @jest-environment jsdom
+ */
+
+import { render, screen } from '@testing-library/react';
+// import { JSDOM } from 'jsdom';
 import App from './App';
 
 describe('App initial tests', () => {
-  beforeEach(() => {
+  /* beforeEach(() => {
     const dom = new JSDOM();
     global.window = dom.window as any;
     global.document = dom.window.document;
-  });
+  }); */
 
   it('renders without crashing', () => {
     render(<App />);
+  });
+
+  it('has the same number of cards as there are in the cat.js file', () => {
+    render(<App />);
+    const card = screen.getAllByRole('article');
+    expect(card).toHaveLength(10);
   });
 });
