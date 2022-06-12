31a33d689c3a593c5b4ec2eefcc4e8b6676f28cf
Nic <42087659+nicm42@users.noreply.github.com>
2022-06-12 17:24:03 +0100

    Tested App renders correctly

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import { render } from '@testing-library/react';
+import { JSDOM } from 'jsdom';
 import App from './App';
 
 describe('App initial tests', () => {
+  beforeEach(() => {
+    const dom = new JSDOM();
+    global.window = dom.window as any;
+    global.document = dom.window.document;
+  });
+
   it('renders without crashing', () => {
-    // render(<App />);
-    expect(true).toBeTruthy();
+    render(<App />);
   });
 });
